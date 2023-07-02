4aeba3b14f09b4ba6af5547d7b03183b10de4a04
suza123456 <mahrjansuja222@gmail.com>
2023-07-02 13:28:07 +0545

    add gender and store in localstorage

--- assets/scripts/characterCreation/main.js
+++ assets/scripts/characterCreation/main.js
@@ -1,20 +1,15 @@
+import { CharacterData } from "./CharacterData.js";
+
 $(document).ready(function() {
+    const characterdata = new CharacterData();
+
+    // addGender();
+    // addclassGroup();
+    // getCharacterData("characters");
     $('[data-toggle="tooltip"]').tooltip();
 
     $("#party-name-btn").click(() => {
-        console.log($(".username"));
-        let usernames = [$('#username-0'), $('#username-1'), $('#username-2')];
-        let party_name = $('#party-name-input').val();
-        usernames.forEach((username, index) => {
-            console.log(username.val());
-            if (username.val().length == 0) {
-                alert(username.attr("id"));
-            }
-        });
-        if (party_name.length == 0) {
-            alert("tero name khali xa bee");
-        } else {
-            location.href = "seedGeneration.html";
-        }
+
+        characterdata.addcharacterData();
     });
 });
