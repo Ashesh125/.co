55e8df36243da032baf2d3ee72a7df00e2747acd
suza123456 <mahrjansuja222@gmail.com>
2023-07-06 18:57:27 +0545

    create modal

--- assets/scripts/world/main.js
+++ assets/scripts/world/main.js
@@ -1,10 +1,10 @@
 import NoiseGenerator from './NoiseGenerator.js';
-import {World} from './World.js';
+import { World } from './World.js';
 import { popUp } from '../helpers/Helper.js';
 import { CONSTANTS } from '../constants/Constant.js';
 import { Save } from "../save/Save.js";
 
-$(document).ready(function(){
+$(document).ready(function() {
     // const game = {
     //   "id" : 2,
     //   "name": "Test Save 2",
@@ -24,51 +24,43 @@ $(document).ready(function(){
 
     let loadSave = true;
 
-    if(loadSave){
-      $("#title-main").text(game.name+".co");
-      var saveObj = new Save(game);
-      let save = saveObj.getSave();
+    if (loadSave) {
+        $("#title-main").text(game.name + ".co");
+        var saveObj = new Save(game);
+        let save = saveObj.getSave();
 
-      const noiseGenerator = new NoiseGenerator(save.world.seed);
-      const world = new World(noiseGenerator);
-      world.loadSaveState(save);
+        const noiseGenerator = new NoiseGenerator(save.world.seed);
+        const world = new World(noiseGenerator);
+        world.loadSaveState(save);
 
-      $(document).keydown(function (event) {
-        // console.log("key:"+event.key);
-        world.action(event.key);
-      });
+        $(document).keydown(function(event) {
+            // console.log("key:"+event.key);
+            world.action(event.key);
+        });
 
-      $('.save-button').on('click',function(){      
-        saveObj.saveGame(world);
-        popUp("Game has been Saved");
-      });
+        $('.save-button').on('click', function() {
+            saveObj.saveGame(world);
+            popUp("Game has been Saved");
+        });
 
-      $('.save-quit-button').on('click',function(){
-        saveObj.saveGame(world);
-        popUp("Game has been Saved");
-        localStorage.removeItem("towns");
-        localStorage.removeItem("characters");
-        localStorage.removeItem("gameState");
-        window.location.href = "./mainpage.html";
-      });
+        $('.save-quit-button').on('click', function() {
+            saveObj.saveGame(world);
+            popUp("Game has been Saved");
+            window.close();
+        });
 
-      $('.quit-button-first').on('click',function(){
-          $("#confirmModal").modal('toggle');
-      });
+        $('.quit-button').on('click', function() {
+            $("#confirmModal").modal('toggle');
+        });
 
-      $(".quit-button-second").on('click',function(){
-
-        localStorage.removeItem("towns");
-        localStorage.removeItem("characters");
-        localStorage.removeItem("gameState");
-        window.location.href = "./mainpage.html";
-      });
+        $(".exit-button").on('click', function() {
+            window.close();
+        });
     } else {
 
     }
 
 
 
- });
 
- 
+});
